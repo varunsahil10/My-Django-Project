d3c3bd76717524b3bb359215ac873f8fe372553a
varun <varunsahil10@gmail.com>
2025-01-09 21:36:36 +0530

    Add context to index.html from index()

--- home/views.py
+++ home/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    names = ['John', 'Doe', 'Jane']
+    context = {
+        'names': names
+    }
+    return render(request, 'index.html', context)
 
 def contact(request):
     return render(request, 'contact.html')
