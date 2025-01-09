42f1f84bc140e01e88b0e85b953de0f91ff06999
varun <varunsahil10@gmail.com>
2025-01-09 15:07:44 +0530

    Add dynamic route

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from home.views import index, contact
+from home.views import index, contact,dynamic_route
 
 urlpatterns = [
     path('', index),
-    path('contact/', contact)
+    path('contact/', contact),
+    path('dynamic_route/<int:number>', dynamic_route),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,10 @@ def index(request):
 
 def contact(request):
     return HttpResponse("This is Contact page!!!📞")
+
+def dynamic_route(request,number):
+    result = f"Table of {number} is :"
+    for i in range(1,11):
+       result += f"\n {number} * {i} = {number * i}"
+    print(result) 
+    return HttpResponse(result)
