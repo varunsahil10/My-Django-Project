45dd7c24913623c69ef2864e393e2049563610df
varun <varunsahil10@gmail.com>
2025-01-12 03:18:00 +0530

    Add widgets and access labels from backend

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Student
+from django.utils.translation import gettext_lazy as _
 
 # class StudentForm(forms.Form):
 #     name = forms.CharField()
@@ -9,4 +10,19 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = '__all__'
-        
+        labels = {
+            'name': 'Student Name',
+            'roll': 'Student Roll',
+        }
+        help_texts = {
+            "name": _("Some useful help text."),
+        }
+        error_messages = {
+            "name": {
+                "max_length": _("This writer's name is too long."),
+            },
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'roll': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
