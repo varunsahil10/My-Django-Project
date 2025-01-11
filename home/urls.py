23da143ff6a271a82d1e3dce168ca27198a96f61
varun <varunsahil10@gmail.com>
2025-01-11 22:06:34 +0530

    Extend base.html to index and contact page

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from home.views import index, contact,dynamic_route
 
 urlpatterns = [
-    path('', index),
-    path('contact/', contact),
+    path('', index,name='home'),
+    path('contact/', contact,name='contact'),
     path('dynamic_route/<int:number>', dynamic_route),
 ]
