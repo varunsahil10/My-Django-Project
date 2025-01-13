e0c68f5de5acef1a283ef9a4de34f0cf10cf541c
varun <varunsahil10@gmail.com>
2025-01-14 00:26:09 +0530

    Add fake teachers

--- home/seed_db.py
+++ home/seed_db.py
@@ -4,10 +4,28 @@ from random import randint
 
 fake = Faker('en_IN')
 
-def generate_students():
-    for _ in range(10):
+def generate_students(num = 10):
+    for _ in range(num):
         student = Student(
             name=fake.name(), 
             roll=randint(1, 100),
         )
         student.save()
+
+
+def generate_teachers(num = 10):
+    for _ in range(num):
+        teacher = Teacher(
+            name=fake.name(),
+            subject=fake.word(),
+        )
+        teacher.student = Student.objects.all()[randint(0, Student.objects.count()-1)]
+        teacher.save()
+
+# def generate_courses(num = 10):
+#     for _ in range(num):
+#         course = Course(
+#             name=fake.word(),
+#         )
+#         course.student = Student.objects.all()[randint(0, Student.objects.count()-1)]
+#         course.save()
