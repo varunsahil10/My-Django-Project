a16b6a4ec3de621c94c58a1dc6af1acbb3927311
varun <varunsahil10@gmail.com>
2025-02-04 22:41:52 +0530

    Add Debug Toolbar

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -40,12 +40,15 @@ INSTALLED_APPS = [
 
     'home',
     'django_extensions',
+    "debug_toolbar",
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -53,6 +56,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'firstproject.urls'
+
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 TEMPLATES = [
     {
--- firstproject/urls.py
+++ firstproject/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home import urls as home_urls
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(home_urls)),
-]
+] + debug_toolbar_urls()
